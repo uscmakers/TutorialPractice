7bca814a60332f57476f68c3270be973ac010f3e
cflenoury <cflenour@usc.edu>
2023-01-29 19:41:33 -0800

    Cleaning repo

--- Anjali_Gopinathan.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout << "Anjali Gopinathan" << endl;
-    return 0;
-}
--- isaac.py
+++
@@ -1,8 +0,0 @@
-name = input("What is Your Name? ")
-
-if name == "Isaac":
-    print("Yes")
-
-else:
-    print("You aren't Isaac")
-    
